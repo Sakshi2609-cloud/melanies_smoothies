ff936f61d493a680288de1bb3e586036722a4c77
Sakshi2609-cloud <sakshi.singh21@infosys.com>
2024-11-07 18:37:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,8 @@ if ingredients_list:
                 
     #st.write(ingredients_string)
 
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-        values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+               my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+               values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
     st.write(my_insert_stmt)
     st.stop
@@ -44,7 +44,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-
-
-
-
